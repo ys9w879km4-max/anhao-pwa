98dc2e9d548e1fa77c6039345b2e1d3f27059579
ys9w879km4-max <ys9w879km4@privaterelay.appleid.com>
2026-08-19 12:44:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'anhao-pwa-v16';
+const CACHE = 'anhao-pwa-v17';
 const ASSETS = ['./','./index.html','./manifest.json','./sw.js','./icon-192.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
